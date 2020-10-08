3ec92cf9c602a8d957107c0adbbef6289c7d9821
Gelar Pambudi <gelargelur@gmail.com>
2020-10-08 23:12:17 +0700

    add null handler in hello.php

--- test-code/hello.php
+++ test-code/hello.php
@@ -1,4 +1,11 @@
 <?php
     $name = $_GET['name'];
-    echo "Hello $name, the server is up :)";
+
+    if (isset($name)) {
+        echo "Hello $name, the server is up :)";
+    } 
+    else {
+        echo "Whoops i don't know your name, but the server is up tho :)";
+    }
+    
 ?>

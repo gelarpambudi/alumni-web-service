f565ba14ab1bf9c5a854809174ef8fe41fece8d8
Gelar Pambudi <gelargelur@gmail.com>
2020-10-08 23:31:16 +0700

    rework hello.php

--- test-code/hello.php
+++ test-code/hello.php
@@ -1,11 +1,11 @@
 <?php
-    $name = $_GET['name'];
 
-    if (isset($name)) {
-        echo "Hello $name, the server is up :)";
+    if (isset($_GET['name'])) {
+        $name = $_GET['name'];
+        echo "<h2> Hello $name, the server is up :) </h2>";
     } 
     else {
-        echo "Whoops i don't know your name, but the server is up tho :)";
+        echo "<h2> Whoops i don't know your name, but the server is up tho :) </h2>";
     }
     
 ?>

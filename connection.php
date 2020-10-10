d46bc28d72d8790e64c7e3c73e0b7b2cffcf0dd7
Gelar Pambudi <gelargelur@gmail.com>
2020-10-10 21:37:47 +0700

    edit connection

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
  define('HOST','localhost');
- define('USER','id15073549_db_alumni_admin');
- define('PASS','#K4rL4rLTKAGG');
- define('DB','id15073549_db_alumni');
+ define('USER',' ');
+ define('PASS',' ');
+ define('DB',' ');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
  
